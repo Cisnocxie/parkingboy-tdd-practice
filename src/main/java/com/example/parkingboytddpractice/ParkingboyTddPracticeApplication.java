4a3b023bc695a9673db1403796122467652b80d0
XIECI <cisnoc.xie@oocl.com>
2018-08-23 16:26:30 +0800

    [leo cisnoc] test user controller post a user

---
+++ src/main/java/com/example/parkingboytddpractice/ParkingboyTddPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.parkingboytddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingboyTddPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingboyTddPracticeApplication.class, args);
+	}
+}
