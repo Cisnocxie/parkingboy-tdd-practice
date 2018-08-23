da6943792d79326e3aadabb41252895c0fd2af90
XIECI <cisnoc.xie@oocl.com>
2018-08-23 18:35:38 +0800

    [leo cisnoc] test user controller test returned password

--- src/test/java/com/example/parkingboytddpractice/controllers/UserControllerTest.java
+++ src/test/java/com/example/parkingboytddpractice/controllers/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.example.parkingboytddpractice.entities.User;
 import com.example.parkingboytddpractice.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,9 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -32,7 +36,7 @@ public class UserControllerTest {
     private ObjectMapper mapper;
 
     @Test
-    public void should_create_new_user_when_give_a_user() throws Exception {
+    public void should_createUser_when_give_a_user() throws Exception {
         User user = new User();
         user.setUsername("newuser");
         when(userService.createUser(any(User.class))).thenReturn("123");
@@ -42,5 +46,19 @@ public class UserControllerTest {
                 .content(mapper.writeValueAsString(user)));
 
         result.andExpect(status().isCreated()).andDo(print());
+        String contentAsString = result.andReturn().getResponse().getContentAsString();
+        Assertions.assertEquals("123",contentAsString);
     }
+
+    @Test
+    public void should_return_400_when_createUser_given_null() throws Exception {
+        when(userService.createUser(any(User.class))).thenReturn("123");
+
+        ResultActions result = mvc.perform(post("/users"));
+
+        result.andExpect(status().is4xxClientError()).andDo(print());
+    }
+
+
+
 }
